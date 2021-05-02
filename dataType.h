b5320a686e0224b2b393e21fca8127e50562d679
Nikolaus J. Reichert <nikolaus.reichert@umsats.ca>
2021-05-02 14:24:26 -0500

    Added datatypes.

---
+++ dataType.h
@@ -0,0 +1,34 @@
+typedef struct {
+    char priority;
+    char id;
+    char cmd;
+    char args[7];
+    char outgoing; /* 0 == incoming. 1 == outgoing. */
+} Command_t;
+
+/* Ripped from STM32 libs */
+typedef struct
+{
+  unsigned int StdId;    /*!< Specifies the standard identifier.
+                          This parameter must be a number between Min_Data = 0 and Max_Data = 0x7FF. */
+
+  unsigned int ExtId;    /*!< Specifies the extended identifier.
+                          This parameter must be a number between Min_Data = 0 and Max_Data = 0x1FFFFFFF. */
+
+  unsigned int IDE;      /*!< Specifies the type of identifier for the message that will be transmitted.
+                          This parameter can be a value of @ref CAN_identifier_type */
+
+  unsigned int RTR;      /*!< Specifies the type of frame for the message that will be transmitted.
+                          This parameter can be a value of @ref CAN_remote_transmission_request */
+
+  unsigned int DLC;      /*!< Specifies the length of the frame that will be transmitted.
+                          This parameter must be a number between Min_Data = 0 and Max_Data = 8. */
+
+  unsigned int Timestamp; /*!< Specifies the timestamp counter value captured on start of frame reception.
+                          @note: Time Triggered Communication Mode must be enabled.
+                          This parameter must be a number between Min_Data = 0 and Max_Data = 0xFFFF. */
+
+  unsigned int FilterMatchIndex; /*!< Specifies the index of matching acceptance filter element.
+                          This parameter must be a number between Min_Data = 0 and Max_Data = 0xFF. */
+
+} CAN_RxHeaderTypeDef;
