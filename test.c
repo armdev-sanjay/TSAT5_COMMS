33624fa53de8a21bbb9217c977e15a6df47c6efc
Nikolaus J. Reichert <nikolaus.reichert@umsats.ca>
2021-05-20 13:34:53 -0500

    Nulled out queue.

--- test.c
+++ test.c
@@ -11,6 +11,11 @@ int main(void)
 
     Command_t *queue[QUEUE_SIZE];
 
+    for (size_t i = 0; i < QUEUE_SIZE; i++) {
+        queue[i] = NULL;
+    }
+    
+
     for (size_t i = 0; i < QUEUE_SIZE; i++)
     {
         Command_t *tempCommand = (Command_t *)malloc(sizeof(Command_t));
@@ -38,6 +43,14 @@ int main(void)
     };
 
     enqueue(queue, &cmd);
+
+    Command_t cmdTwo = {
+        .priority = 12,
+        .id = 35,
+        .cmd = 0x43,
+        .args = {0x15, 0x44, 0x00, 0x00, 0x05, 0xff, 0x88},
+        .outgoing = 1
+    };
 
     return 0; 
 }
